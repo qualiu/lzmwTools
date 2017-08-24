1425b6faa18da3a54d401d3eaab03f7e7f7ead5e
qualiu <qualiu@microsoft.com>
2017-08-24 14:54:47 +0800

    Last time to sync https://github.com/qualiu/msrTools , please use https://github.com/qualiu/msrTools from now on.

--- disable-exe-in-PATH.bat
+++ disable-exe-in-PATH.bat
@@ -2,26 +2,33 @@
 :: Find and disable specified exe files in PATH
 ::===============================================
 @echo off
-where lzmw.exe 2>nul >nul || if not exist %~dp0\lzmw.exe powershell -Command "Invoke-WebRequest -Uri https://github.com/qualiu/lzmw/blob/master/tools/lzmw.exe?raw=true -OutFile %~dp0\lzmw.exe"
-where lzmw.exe 2>nul >nul || set "PATH=%PATH%;%~dp0"
+
+SetLocal EnableExtensions EnableDelayedExpansion
+
+set lzmwExe=%~dp0lzmw.exe
+if not exist %lzmwExe% powershell -Command "Invoke-WebRequest -Uri https://github.com/qualiu/lzmw/blob/master/tools/lzmw.exe?raw=true -OutFile %~dp0lzmw.exe"
 
 if "%~1" == "" (
-    echo Usage  : %~n0  ExeFilePattern      | lzmw -aPA -e "%~n0\s+(\S+).*"
-    echo Example: %~n0  lzmw.exe            | lzmw -aPA -e "%~n0\s+(\S+).*"
-    echo Example: %~n0  "^lzmw\.exe$"       | lzmw -aPA -e "%~n0\s+(\S+).*"
-    echo Example: %~n0  "^(lzmw|nin)\.exe$" | lzmw -aPA -e "%~n0\s+(\S+).*"
+    echo Usage  : %~n0  ExeFilePattern     | %lzmwExe% -aPA -e "%~n0\s+(\S+).*"
+    echo Example: %~n0  lzmw.exe            | %lzmwExe% -aPA -e "%~n0\s+(\S+).*"
+    echo Example: %~n0  "^(lzmw|nin)\.exe$"       | %lzmwExe% -aPA -e "%~n0\s+(\S+).*"
+    echo Example: %~n0  "^(lzmw|nin)\.exe$|psall.bat" | %lzmwExe% -aPA -e "%~n0\s+(\S+).*"
     exit /b -1
 )
 
-where nin.exe 2>nul >nul || if not exist %~dp0\nin.exe powershell -Command "Invoke-WebRequest -Uri https://github.com/qualiu/lzmw/blob/master/tools/nin.exe?raw=true -OutFile %~dp0\nin.exe"
-where nin.exe 2>nul >nul || set "PATH=%PATH%;%~dp0"
+set ninExe=%~dp0nin.exe
+if not exist %ninExe% powershell -Command "Invoke-WebRequest -Uri https://github.com/qualiu/lzmw/blob/master/tools/nin.exe?raw=true -OutFile %~dp0nin.exe"
 
-:: Dispaly files with pattern %1
-lzmw -l -p "%PATH%" -f %1 --wt --sz -M 2>nul
+:: Dispaly files with exe pattern %1
+%lzmwExe% -l -f "%~1" --wt --sz -p "%PATH%" 2>nul
+if %ERRORLEVEL% EQU 0 exit /b 0
 
-for /f "tokens=*" %%a in ('lzmw -l -p "%PATH%" -f %1 -PAC 2^>nul ^| nin nul "^(.+)[\\/][^\\/]*$" -iuPAC') do (
-    for /f "tokens=*" %%p in ('lzmw -z "%PATH%" -ix "%%a;" -o "" -aPAC 2^>nul') do set "PATH=%%p"
-    lzmw -l -p "%PATH%" -f %1 -PAC >nul 2>nul || for /f "tokens=*" %%p in ('lzmw -z "%PATH%" -ix "%%a\\;" -o "" -aPAC 2^>nul') do set "PATH=%%p"
-    lzmw -l -p "%PATH%" -f %1 -PAC >nul 2>nul || for /f "tokens=*" %%p in ('lzmw -z "%PATH%" -ix ";%%a\\" -o "" -aPAC 2^>nul') do set "PATH=%%p"
-    lzmw -l -p "%PATH%" -f %1 -PAC >nul 2>nul || for /f "tokens=*" %%p in ('lzmw -z "%PATH%" -ix ";%%a" -o "" -aPAC 2^>nul') do set "PATH=%%p"
+set "tmpPATH=%PATH%"
+for /f "tokens=*" %%a in ('%lzmwExe% -l -f "%~1" -PAC 2^>nul -p "%PATH%" ^| %ninExe% nul "^([a-z]+.+?)[\\/][^\\/]*$" -iuPAC') do (
+    :: echo Will remove in PATH: %%a
+    for /f "tokens=*" %%b in ('%lzmwExe% -z "!tmpPATH!" -t "\\*\s*;\s*" -o "\n" -aPAC ^| %lzmwExe% --nx %%a -i -PAC ^| %lzmwExe% -S -t "[\r\n]+\s*(\S+)" -o ";$1" -aPAC ^| %lzmwExe% -S -t "\s+$" -o "" -aPAC') do (
+        set "tmpPATH=%%b"
+    )
 )
+
+EndLocal & set "PATH=%tmpPATH%"
